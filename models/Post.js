54af7290ac423ff495c255c6da6d545076e60631
Neema Velasco <neemavelasco@gmail.com>
2022-04-11 15:41:26 -0500

    added user and post models

---
+++ models/Post.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdAt: {
+        type: "TIMESTAMP",
+        defaultValue: DataTypes.literal("CURRENT_TIMESTAMP"),
+        allowNull: false,
+      },
+    creator_id: {
+      type: DataTypes.BOOLEAN,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
