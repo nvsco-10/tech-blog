7dc1e06eb1ef5e74c240dd81e14c564e5008e309
Neema Velasco <neemavelasco@gmail.com>
2022-04-11 15:23:05 -0500

    added main html template in views and css in public folder

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require('./controllers');
 // const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +15,8 @@ const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
