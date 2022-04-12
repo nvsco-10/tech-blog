f8c1c2cad0ea77f5cfef9c8c5ab2317186a842db
Neema Velasco <neemavelasco@gmail.com>
2022-04-11 20:06:02 -0500

    added route to get single post by id

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,14 +1,58 @@
 const router = require('express').Router();
-const Post = require('../models/Post');
+const { User, Post } = require('../models');
 
 // route to get all blog posts
 router.get('/', async (req, res) => {
-  const blogPosts = await Post.findAll().catch((err) => { 
-      res.json(err);
+  try {
+    const blogPosts = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
     });
-      const posts = blogPosts.map((post) => post.get({ plain: true }));
-      res.render('homepage', { posts });
+
+    const posts = blogPosts.map((post) =>
+      post.get({ plain: true })
+    );
+
+    res.render('homepage', {
+      posts,
+      loggedIn: req.session.loggedIn,
     });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+
+});
+
+router.get('/post/:id', async (req, res) => {
+  // If the user is not logged in, redirect the user to the login page
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+    // If the user is logged in, allow them to view the post
+    try {
+      const singlePost = await Post.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: [
+              'name',
+            ],
+          },
+        ],
+      });
+      const post = singlePost.get({ plain: true });
+      res.render('post', { post, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+});
 
 
 module.exports = router;
