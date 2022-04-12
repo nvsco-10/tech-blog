f1dd3c7a1e30d14f88ccb7c5211305991d98ddfd
Neema Velasco <neemavelasco@gmail.com>
2022-04-12 11:04:49 -0500

    added create new post page and route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -9,5 +9,9 @@ router.get('/newpost', async (req, res) => {
     res.render('newpost', { loggedIn: req.session.loggedIn })
 });
 
+router.post('/newpost', async (req,res) => {
+    console.log("post")
+})
+
 
 module.exports = router;
--- public/js/new-post.js
+++ public/js/new-post.js
@@ -1,15 +1,15 @@
-const newPost = async () => {
+const addPost = async () => {
     const response = await fetch('/dashboard/newpost', {
-      method: 'GET',
+      method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
   
     if (response.ok) {
-      document.location.replace('/dashboard/newpost');
+      document.location.replace('/');
     } else {
       alert('Failed to log out.');
     }
   };
   
-  document.querySelector('#logout').addEventListener('click', newPost);
+  document.querySelector('#add-post').addEventListener('click', addPost);
   
