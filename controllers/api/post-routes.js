3a1cbb1d9984278a08e20e86e9184d98e8eb4b0c
Neema Velasco <neemavelasco@gmail.com>
2022-04-12 19:33:11 -0500

    added post delete route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -19,24 +19,42 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-
-    try {
-        const updatedPost = await Post.update({
-          title: req.body.title,
-          content: req.body.content
-        },
-        {
-          where: {
-            id: req.params.id
-          }
-        });
-      
-        res.json(updatedPost);
-        } catch (err) {
-          console.log(err);
-          res.status(500).json(err);
+  try {
+      const updatedPost = await Post.update({
+        title: req.body.title,
+        content: req.body.content
+      },
+      {
+        where: {
+          id: req.params.id
         }
+      });
     
-  });
+      res.json(updatedPost);
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+  
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedPost = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedPost) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(deletedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,12 +1,12 @@
+//get post id using window.location
+const pathLength = window.location.pathname.split('/').length;
+const postId = window.location.pathname.split('/')[pathLength - 1];
+
 const updatePost = async (event) => {
     event.preventDefault();
   
     const title = document.querySelector('#edit-title').value;
     const content = document.querySelector('#edit-content').value;
-
-    //get post id using window.location
-    const pathLength = window.location.pathname.split('/').length;
-    const postId = window.location.pathname.split('/')[pathLength - 1];
   
     if (title && content) {
   
@@ -23,7 +23,20 @@ const updatePost = async (event) => {
       }
   
     };
-  
   }
-    
-    document.querySelector('#updatepost').addEventListener('click', updatePost);
+
+  const delPost = async (event) => {
+    const response = await fetch(`/api/posts/${postId}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post.');
+    }
+}
+
+
+document.querySelector('#updatepost').addEventListener('click', updatePost);
+document.querySelector('#deletepost').addEventListener('click', delPost);
